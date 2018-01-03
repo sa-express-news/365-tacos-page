131c8c90cb7295755cad983f50cdffa61c6f9a30
Kia Farhang <kfarhang0@gmail.com>
2018-01-03 10:55:24 -0600

    Add file to hold all types/interfaces. Create interface for Restaurant component

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Restaurant {
+    name: string;
+    review: string;
+    address: string;
+    otherLocations: string;
+    phone: string;
+    website: string;
+    latitude: number;
+    longitude: number;
+    image: string;
+}
