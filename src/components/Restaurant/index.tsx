d1a4506ed1db1741a5e3f3121b878cc52b56f0ff
Kia Farhang <kfarhang0@gmail.com>
2018-01-03 11:06:51 -0600

    Add basic Restaurant component

---
+++ src/components/Restaurant/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Restaurant as Props } from '../../types';
+
+const Restaurant = ({ name, review, address, otherLocations, phone, website, latitude, longitude, image }: Props) => {
+    return (
+        <div className="Restaurant">
+            <a href={review} title={name}>
+                <img src={image} alt={name} />
+            </a>
+            <h4>{name}</h4>
+            <p>{address}</p>
+            <p><a href={`tel:+1${phone}`}></a>{phone}</p>
+            <p>{website}</p>
+        </div>
+    )
+}
