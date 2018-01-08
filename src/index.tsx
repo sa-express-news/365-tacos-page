7a3176503b089ec0bd63a3b7b75c9a0e5e366170
Kia Farhang <kfarhang0@gmail.com>
2018-01-08 17:05:54 -0600

    Add redux wiring to app entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+import { tacoApp, initialState } from './reducers';
+import { AppState } from './types';
+
+import { Provider } from 'react-redux';
 import App from './components/App/';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = createStore(tacoApp, initialState);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
