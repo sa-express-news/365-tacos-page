72d90c6fd4c4cbfbf25234885f9268687e386e19
Kia Farhang <kfarhang0@gmail.com>
2018-01-08 17:04:24 -0600

    Add reducers directory. Add declaration of initial state, overarching app reducer and a reducer to set the current search term

---
+++ src/reducers/index.tsx
@@ -0,0 +1,32 @@
+import { combineReducers } from 'redux'
+
+import * as types from '../types';
+import * as constants from '../constants';
+import * as actions from '../actions';
+
+const restaurants: types.Restaurant[] = require('../tacos.json');
+
+export const initialState: types.AppState = {
+    restaurants,
+    location: {
+        latitude: 0,
+        longitude: 0
+    },
+    activeFilter: 'SHOW_ALL',
+    currentSearch: ''
+};
+
+export const currentSearch = (action: actions.SetSearchTerm): string => {
+    if (action.term) {
+        return action.term;
+    }
+    else return '';
+}
+
+export const tacoApp = (state: types.AppState = initialState, action: actions.Action): types.AppState => {
+    switch (action.type) {
+        case constants.SET_SEARCH_TERM:
+            return Object.assign({}, state, { currentSearch: currentSearch(action as actions.SetSearchTerm) })
+    }
+    return state;
+}
