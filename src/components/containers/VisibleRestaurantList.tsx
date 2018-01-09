6d8baa2aaca7bdaee399f090d190499ed235ad7c
Kia Farhang <kfarhang0@gmail.com>
2018-01-09 15:56:55 -0600

    Update VisibleRestaurantList to filter based on text search string in state

--- src/components/containers/VisibleRestaurantList.tsx
+++ src/components/containers/VisibleRestaurantList.tsx
@@ -2,12 +2,15 @@ import { connect } from 'react-redux';
 import RestaurantList from '../RestaurantList';
 import * as types from '../../types';
 
-const getVisibleRestaurants = (restaurants: types.Restaurant[], filter: string, location: types.LatLong | null): types.Restaurant[] => {
+const getVisibleRestaurants = (restaurants: types.Restaurant[], currentSearch: string, location: types.LatLong | null): types.Restaurant[] => {
+    if (currentSearch !== '') {
+        return restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(currentSearch.toLowerCase()));
+    }
     return restaurants;
 }
 
 const mapStateToProps = (state: types.AppState) => ({
-    restaurants: getVisibleRestaurants(state.restaurants, state.activeFilter, state.location)
+    restaurants: getVisibleRestaurants(state.restaurants, state.currentSearch, state.location)
 });
 
 const VisibleRestaurantList = connect(
