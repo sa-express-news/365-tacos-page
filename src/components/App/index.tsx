935b005629f8b75e673ed4f228d970f1047a8ec0
Kia Farhang <kfarhang0@gmail.com>
2018-01-15 17:10:47 -0600

    Refactor top-level App component to consume AppHeader and VisibleRestaurantList

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import AppHeader from '../AppHeader';
+import VisibleRestaurantList from '../containers/VisibleRestaurantList';
+
+import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-
+      <AppHeader />
+      <VisibleRestaurantList />
     </div>
   )
 }
