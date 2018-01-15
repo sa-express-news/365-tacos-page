247f42ef0cd0eef86f114bb9015c72f4fcedcbcf
Kia Farhang <kfarhang0@gmail.com>
2018-01-15 14:36:57 -0600

    Add filtering by location, location + search term to VisibleRestaurantList container component

--- src/components/containers/VisibleRestaurantList.tsx
+++ src/components/containers/VisibleRestaurantList.tsx
@@ -2,11 +2,46 @@ import { connect } from 'react-redux';
 import RestaurantList from '../RestaurantList';
 import * as types from '../../types';
 
+import { getGreatCircleDistanceInKM } from '../../util';
+import { LatLong } from '../../types';
+
 const getVisibleRestaurants = (restaurants: types.Restaurant[], currentSearch: string, location: types.LatLong | null): types.Restaurant[] => {
-    if (currentSearch !== '') {
-        return restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(currentSearch.toLowerCase()));
+
+    if (currentSearch === '' && !location) {
+        return restaurants;
+    } else {
+
+        const filterBySearch = (restaurants: types.Restaurant[]): types.Restaurant[] => {
+            return restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(currentSearch.toLowerCase()));
+        }
+
+        const filterByLocation = (restaurants: types.Restaurant[]): types.Restaurant[] => {
+            // First, we create a new restaurant array with distance to current location calculated.
+
+            const restaurantsWithDistancesCalculated = restaurants.map((restaurant) => {
+                const restaurantLatLong: types.LatLong = {
+                    latitude: restaurant.latitude,
+                    longitude: restaurant.longitude
+                }
+                return Object.assign({}, restaurant, { distance: getGreatCircleDistanceInKM(location as LatLong, restaurantLatLong) });
+            });
+
+            // Then, we sort that array on distance to current location and return it.
+
+            return restaurantsWithDistancesCalculated.sort((a: types.Restaurant, b: types.Restaurant): number => {
+                return (a.distance as number) - (b.distance as number);
+            });
+        }
+
+        let restaurantsToReturn = restaurants;
+
+        if (currentSearch !== '') restaurantsToReturn = filterBySearch(restaurantsToReturn);
+
+        if (location !== null) restaurantsToReturn = filterByLocation(restaurantsToReturn);
+
+        return restaurantsToReturn;
+
     }
-    return restaurants;
 }
 
 const mapStateToProps = (state: types.AppState) => ({
