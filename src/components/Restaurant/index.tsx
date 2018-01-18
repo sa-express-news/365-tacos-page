3bd95c242fc0ea5a611807d95c24a3a36b3e06a2
Kia Farhang <kfarhang0@gmail.com>
2018-01-17 18:09:01 -0600

    Remove website, phone number from each restaurant, replace with a link to our review

--- src/components/Restaurant/index.tsx
+++ src/components/Restaurant/index.tsx
@@ -14,10 +14,11 @@ const Restaurant = ({ name, review, address, otherLocations, phone, website, lat
             <div className="info">
                 <h4>{name}</h4>
                 <p><i className="fas fa-map-marker-alt"></i> {address}</p>
-                <p><a href={`tel:+1${phone}`}><i className="fas fa-phone"></i> {phone}</a></p>
-                {website !== 'None' &&
+                {/* <p><a href={`tel:+1${phone}`}><i className="fas fa-phone"></i> {phone}</a></p> */}
+                {/* {website !== 'None' &&
                     <p><a href={website} title={name}> <i className="fas fa-external-link-alt"></i> Website</a></p>
-                }
+                } */}
+                <p><a href={review} title={name}><i className="fas fa-pencil-alt"></i> Read our review</a></p>
                 {distance &&
                     <p>{precisionRound(distance * 0.621371, 1)} mi</p>
                 }
