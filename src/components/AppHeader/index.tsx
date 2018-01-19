c28ab9c8bcab5d97859ca21b101485e89e7857a6
Kia Farhang <kfarhang0@gmail.com>
2018-01-19 11:12:19 -0600

    Update AppHeader to include Credits

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import SearchInput from '../containers/SearchInput';
 import NearMeButton from '../containers/NearMeButton';
+import ShowCreditsButon from '../containers/ShowCreditsButton';
+import VisibleCredits from '../containers/VisibleCredits';
 
 import './AppHeader.css';
 
@@ -9,7 +11,7 @@ const AppHeader = () => {
     return (
         <div className="AppHeader">
             <h1>The ones that were worth the drive</h1>
-            <p>Looking for the best tacos in San Antonio? Luckily, Express-News food writer Mike Sutter did all the hard work for you.</p>
+            <p>Looking for the best tacos in San Antonio? Luckily, Express-News food writer <a href="/author/mike-sutter/" title="Mike Sutter">Mike Sutter</a> did all the hard work for you.</p>
             <p>In his 365 Days of Tacos series, Mike visted and reviewed a new taco joint daily throughout 2017. We've gathered the best of them: the restaurants Mike deemed "worth a drive." </p>
             <p>Search for your favorite below{showNearMe ? " or click the 'near me' button to see the high-quality tacos closest to you." : "."} </p>
             <div className="input">
@@ -17,6 +19,10 @@ const AppHeader = () => {
                 {showNearMe &&
                     <NearMeButton text="Near me" />
                 }
+            </div>
+            <div>
+                <ShowCreditsButon text="Credits" />
+                <VisibleCredits />
             </div>
         </div>
     )
